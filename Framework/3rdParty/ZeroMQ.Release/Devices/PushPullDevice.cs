73cafb97066f6f1c134d62b89bfff3bcdee6cdfd
mszymczyk <misz.misz.misz@gmail.com>
2016-06-20 21:53:54 +0200

    ZeroMQ C# bindings dll's

---
+++ Framework/3rdParty/ZeroMQ.Release/Devices/PushPullDevice.cs
@@ -0,0 +1,53 @@
+﻿namespace ZeroMQ.Devices
+{
+	using System;
+
+	/// <summary>
+	/// Queuing Push-Pull Device
+	/// </summary>
+	public class PushPullDevice : ZDevice
+	{
+		/// <summary>
+		/// The frontend <see cref="ZSocketType"/> for a streamer device.
+		/// </summary>
+		public static readonly ZSocketType FrontendType = ZSocketType.PULL;
+
+		/// <summary>
+		/// The backend <see cref="ZSocketType"/> for a streamer device.
+		/// </summary>
+		public static readonly ZSocketType BackendType = ZSocketType.PUSH;
+		
+		/// <summary>
+		/// Initializes a new instance of the <see cref="PushPullDevice"/> class.
+		/// </summary>
+		public PushPullDevice(ZContext context)
+			: base(context, FrontendType, BackendType)
+		{ }
+
+		/// <summary>
+		/// Initializes a new instance of the <see cref="StreamerDevice"/> class.
+		/// </summary>
+		public PushPullDevice(ZContext context, string frontendBindAddr, string backendBindAddr)
+			: base(context, FrontendType, BackendType)
+		{
+			FrontendSetup.Bind(frontendBindAddr);
+			BackendSetup.Bind(backendBindAddr);
+		}
+
+		/// <summary>
+		/// Forwards requests from the frontend socket to the backend socket.
+		/// </summary>
+		protected override bool FrontendHandler(ZSocket args, out ZMessage message, out ZError error)
+		{
+			return FrontendSocket.Forward(BackendSocket, out message, out error);
+		}
+
+		/// <summary>
+		/// Not implemented for the <see cref="StreamerDevice"/>.
+		/// </summary>
+		protected override bool BackendHandler(ZSocket args, out ZMessage message, out ZError error)
+		{
+			throw new NotSupportedException();
+		}
+	}
+}
---
+++ Framework/3rdParty/ZeroMQ.Release/ZActor.cs
@@ -0,0 +1,77 @@
+﻿namespace ZeroMQ
+{
+	public delegate void ZAction(ZContext context, ZSocket backend, System.Threading.CancellationTokenSource cancellor, object[] args);
+
+	public class ZActor : ZThread
+	{
+		public ZContext Context { get; protected set; }
+
+		public string Endpoint { get; protected set; }
+
+		public ZAction Action { get; protected set; }
+
+		public object[] Arguments { get; protected set; }
+
+		public ZSocket Backend { get; protected set; }
+
+		public ZSocket Frontend { get; protected set; }
+
+		public ZActor (ZContext context, ZAction action, params object[] args)
+			: this (context, default(string), action, args)
+		{
+			var rnd0 = new byte[8];
+			using (var rng = new System.Security.Cryptography.RNGCryptoServiceProvider()) rng.GetBytes(rnd0);
+			this.Endpoint = string.Format("inproc://{0}", ZContext.Encoding.GetString(rnd0));
+		}
+
+		public ZActor(ZContext context, string endpoint, ZAction action, params object[] args)
+			: base ()
+		{
+			this.Context = context;
+
+			this.Endpoint = endpoint;
+			this.Action = action;
+			this.Arguments = args;
+		}
+
+		protected override void Run()
+		{
+			using (Backend = ZSocket.Create(Context, ZSocketType.PAIR))
+			{
+				Backend.Bind(Endpoint);
+
+				Action(Context, Backend, Cancellor, Arguments);
+			}
+		}
+
+		public override void Start()
+		{
+			base.Start();
+
+			if (Frontend == null)
+			{
+				Frontend = ZSocket.Create(Context, ZSocketType.PAIR);
+				Frontend.Connect(Endpoint);
+			}
+		}
+
+		protected override void Dispose(bool disposing)
+		{
+			base.Dispose(disposing);
+
+			if (disposing)
+			{
+				if (Frontend != null)
+				{
+					Frontend.Dispose();
+					Frontend = null;
+				}
+				if (Backend != null)
+				{
+					Backend.Dispose();
+					Backend = null;
+				}
+			}
+		}
+	}
+}
